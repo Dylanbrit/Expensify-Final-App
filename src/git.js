2c3979e5abb419e96860d0c3e0773eb391b0dd93
unknown <dbrit007@fiu.edu>
2021-07-27 21:23:12 -0400

    trying again again

--- src/git.js
+++ src/git.js
@@ -12,7 +12,7 @@
 // git add takes files from unstaged changes to staged changes
 // git commit then takes our staged changes and makes them commits
 // now we have two git commits- our initial commit, and the changed commits- this is where git is super useful, as we can check on our most updated code or the way it was before we made the changes
-// to initialize our project for github, we need to run git init
+// to initialize our project for github, we need to run git init- this creates a new git repo
 // this creates a .git directory that is managed directly by git
 // git status gives us a look at our files, showing what is untracked
 // we only want git to track the files we wrote
@@ -24,4 +24,10 @@
 // the working tree refers to our files, the ones we edit in VSC
 // the working tree is clean when our files match up with the last commit
 // once we change something in our code, the git status changes
+// git log is a read only command that logs the commits that make up our repository
+// the next step is to get our code on github- here we can collaborate with others, back up our code, and take advantage of all github's features
+// now we need to create a new repository in our github account
+// pick a repository name for the project, description is optional, set to public or private, initialize with a file if we want (probably dont)
+// to push our code to github, copy the HTTPS url, then use git remote add origin pastedurlfromgithub
+// use git push -u (only for the first time we push) origin main- this takes all the local commits to our branch and pushes them to github
 // 
